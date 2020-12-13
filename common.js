c27c90814a250d6b322dbcd4c4c92c78a5c2cad6
Haider Rizvi <53405985+hr080100@users.noreply.github.com>
2020-12-13 21:11:12 +0530

    Add files via upload

---
+++ common.js
@@ -0,0 +1,18 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const line1 = document.querySelector('.line1');
+    const line2 = document.querySelector('.line2');
+    const line3 = document.querySelector('.line3');
+    const menu = document.querySelector('.menu');
+    const footer = document.querySelector('.footer');
+
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('menu-active');
+        line1.classList.toggle('line1-active');
+        line2.classList.toggle('line2-active');
+        line3.classList.toggle('line3-active');
+        footer.classList.toggle('footer-active');
+    });
+}
+
+navSlide();
